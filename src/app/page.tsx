d0ab01df0fc50798e814659e5955b2029c47412f
Dhai <dhaiwatpandya@gmail.com>
2025-10-16 11:56:25 +0530

    remove spotify stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Winamp from "../components/Winamp";
-
 export default function Page() {
   return (
     <main className="mx-auto max-w-screen-md text-center">
@@ -12,28 +10,45 @@ export default function Page() {
       <h1 className="text-4xl font-bold">Dhaiwat Pandya</h1>
       <h3 className="text-xl">dhai.eth</h3>
 
-      <a className="underline text-blue-700" href="https://github.com/dhaiwat10" target="_blank">
+      <a
+        className="underline text-blue-700"
+        href="https://github.com/dhaiwat10"
+        target="_blank"
+      >
         GitHub
       </a>
       <br />
-      <a className="underline text-blue-700" href="https://hackmd.io/@dhaiwat10/ByA1tWTgee" target="_blank">
+      <a
+        className="underline text-blue-700"
+        href="https://hackmd.io/@dhaiwat10/ByA1tWTgee"
+        target="_blank"
+      >
         CV
       </a>
       <br />
-      <a className="underline text-blue-700" href="https://twitter.com/dhaiwat10" target="_blank">
+      <a
+        className="underline text-blue-700"
+        href="https://twitter.com/dhaiwat10"
+        target="_blank"
+      >
         Twitter
       </a>
       <br />
-      <a className="underline text-blue-700" href="https://mirror.xyz/dhaiwat.eth" target="_blank">
+      <a
+        className="underline text-blue-700"
+        href="https://mirror.xyz/dhaiwat.eth"
+        target="_blank"
+      >
         Mirror
       </a>
       <br />
-      <a className="underline text-blue-700" href="https://dhaiwat.substack.com" target="_blank">
+      <a
+        className="underline text-blue-700"
+        href="https://dhaiwat.substack.com"
+        target="_blank"
+      >
         Substack
       </a>
-      <br />
-
-      <Winamp />
     </main>
   );
 }
